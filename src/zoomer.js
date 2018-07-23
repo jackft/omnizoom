a7f39ce4fbd76bc2cd3b8afaf3fbfbae72bb1f46
Jack <jterwill@mit.edu>
2018-07-23 19:40:24 -0400

    added panning

--- src/zoomer.js
+++ src/zoomer.js
@@ -4,9 +4,14 @@ export class Zoomer {
         this.zoomCoef = (opts !== undefined && opts.zoomCoef !== undefined) 
                       ? opts.zoomCoef
                       : 1.1;
+        // zooming
         this.x = 0;
         this.y = 0;
         this.z = 0;
+        // panning
+        this.xDrag = undefined;
+        this.yDrag = undefined;
+        this.dragging = false;
         return this;
     }
 
@@ -45,18 +50,9 @@ export class Zoomer {
         if (zNew < 1) {
             return this;
         }
-
-        // scroll offset
-        const xScroll = window.pageXOffset || document.documentElement.scrollLeft;
-        const yScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-        // container offset
-        const xOffset = elemObj.e.offsetLeft;
-        const yOffset = elemObj.e.offsetTop;
         
         // mouse coordinates
-        const xMouse = x - xOffset + xScroll;
-        const yMouse = y - yOffset + yScroll;
+        const [xMouse, yMouse] = this.clientPosToRelPos(elemObj.e, x, y);
 
         // old zoom coordinates
         const xOld = (xMouse - this.x) / zOld;
@@ -75,8 +71,22 @@ export class Zoomer {
         [this.x, this.y] = this.keepInBounds(elemObj, this.x, this.y, this.zoomCoef**(z));
 
         // make sure scale is to the right of traslate3d
-        const transformation = `translate3D(${this.x}px,${this.y}px,0) scale(${zNew})`;
-        elemObj.e.style.setProperty('transform', transformation);
+        this.setTransformationHelper(elemObj, this.x, this.y, zNew);        
+    }
+
+    clientPosToRelPos(elem, x, y) {
+        // scroll offset
+        const xScroll = window.pageXOffset || document.documentElement.scrollLeft;
+        const yScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        // container offset
+        const xOffset = elem.offsetLeft;
+        const yOffset = elem.offsetTop;
+
+        // relative coordinates
+        const xMouse = x - xOffset + xScroll;
+        const yMouse = y - yOffset + yScroll;
+        return [xMouse, yMouse];
     }
 
     keepInBounds(elemObj, x, y, z) {
@@ -102,7 +112,31 @@ export class Zoomer {
         return [x, y];
     }
 
-    addOnScroll() {
+    pan(x, y) {
+        this.elemObjs.forEach(elemObj => {
+            this.panHelper(elemObj, x, y);
+        });
+        return this;
+    }
+
+    panHelper(elemObj, x, y) {
+        const [xMouse, yMouse] = this.clientPosToRelPos(elemObj.e, x, y);
+        let xx = undefined;
+        let yy = undefined;
+        if (this.xDrag !== undefined && this.yDrag !== undefined) {
+            xx = xMouse - this.xDrag;
+            yy = yMouse - this.yDrag;
+
+           const z = this.zoomCoef**(this.z);
+
+           [this.x, this.y] = this.keepInBounds(elemObj, this.x + xx, this.y + yy, z);
+           this.setTransformationHelper(elemObj, this.x, this.y, z);
+        }
+        this.xDrag = xMouse;
+        this.yDrag = yMouse;
+    }
+
+    addOnScroll(callback) {
         this.elemObjs.forEach(elemObj => {
             elemObj.e.onwheel = (event) => {
                 event.preventDefault();
@@ -110,8 +144,81 @@ export class Zoomer {
                 const y = event.clientY;
                 const zoomIn = (event.deltaY < 0) ? 1 : -1;
                 this.zoom(x, y, zoomIn);
-            };
+            }
+            if (callback !== undefined) {
+                callback(this.x, this.y, this.zoomCoef**this.z);
+            }
         });
+        return this;
+    }
+
+    addOnDrag(callback) {
+        //
+        this.elemObjs.forEach(elemObj => {
+            switch (elemObj.e.tagName) {
+                case "IMG":
+                    elemObj.e.ondrag = (event) => {
+                        const x = event.clientX;
+                        const y = event.clientY;
+                        if (x != 0 && y != 0) {
+                            this.pan(x, y);
+                        }
+                        if (callback !== undefined) {
+                            callback(this.x, this.y, this.zoomCoef**this.z);
+                        }
+                    }
+                    elemObj.e.onmouseup = (event) => {
+                        this.xDrag = undefined;
+                        this.yDrag = undefined;
+                    }
+                    elemObj.e.ondragend = (event) => {
+                        this.xDrag = undefined;
+                        this.yDrag = undefined;
+                    }
+                    elemObj.e.ondragleave = (event) => {
+                        this.xDrag = undefined;
+                        this.yDrag = undefined;
+                    }
+                    break;
+                default:
+                    elemObj.e.onmousemove = (event) => {
+                        const x = event.clientX;
+                        const y = event.clientY;
+                        if (x != 0 && y != 0 && this.dragging) {
+                            this.pan(x, y);
+                        }
+                        if (callback !== undefined) {
+                            callback(this.x, this.y, this.zoomCoef**this.z);
+                        }
+                    }
+                    elemObj.e.onmousedown = (event) => {
+                        this.dragging = true;
+                    }
+                    elemObj.e.onmouseup = (event) => {
+                        this.xDrag = undefined;
+                        this.yDrag = undefined;
+                        this.dragging = false;
+                    }
+                    break;
+            }
+        });
+        return this;
+    }
+
+    getTransformation() {
+        return [this.x, this.y, this.zoomCoef**this.z];
+    }
+
+    setTransformation(x, y, z) {
+        this.elemObjs.forEach(elemObj => {
+            this.setTransformationHelper(elemObj, x, y, z);
+        })
+        return this;
+    }
+
+    setTransformationHelper(elemObj, x, y, z) {
+        const transformation = `translate3D(${x}px,${y}px,0) scale(${z})`;
+        elemObj.e.style.setProperty('transform', transformation);
         return this;
     }
 }
