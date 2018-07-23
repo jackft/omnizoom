1e1c06841f287446914f46f210c319a80950a7fa
Jack <jterwill@mit.edu>
2018-07-23 12:40:46 -0400

    fixed naming

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'zoomy.js',
+    filename: 'zoomy.min.js',
     library: 'zoomy',
     libraryTarget: 'umd'
   }
